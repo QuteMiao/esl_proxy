6db92736b9439f69721187d3ffb8f972e23f6b12
QuteMiao <644122707@qq.com>
2026-07-24 17:06:47 +0800

    add real_chip

--- esl_proxy/src/scheduler/dispatch.c
+++ esl_proxy/src/scheduler/dispatch.c
@@ -20,12 +20,16 @@ void init_ctrl_t(void)
 {
     for (int tid = 0; tid < DISPATCH_THREAD_CNT; tid++) {
         g_ctrl_t[tid].tid = (uint32_t)tid;
-        g_ctrl_t[tid].aicore_mask = AICORE_MASK_PER_DIE;
+        if (AIC_CNT_PER_THREAD >= 64) {
+            g_ctrl_t[tid].aicore_mask = ~0ULL;
+        } else {
+            g_ctrl_t[tid].aicore_mask = ~0ULL >> (64 - AIC_CNT_PER_THREAD);
+        }
 
         // Initialize free_bitmap for TASK_TYPE
         for (int i = 0; i < TASK_TYPE_CNT; i++) {
             for (int j = 0; j < AIC_OSTD; j++) {
-                g_ctrl_t[tid].free_bitmap[i][j] = AICORE_MASK_PER_DIE;
+                g_ctrl_t[tid].free_bitmap[i][j] = g_ctrl_t[tid].aicore_mask;
             }
         }
         // set_mix(tid);
@@ -85,21 +89,63 @@ static inline void set_mix(int tid)
     }
 }
 
-static void hand_shake(int cpu_idx, uint64_t* aicore_spr[], int type) {
-    uint64_t base = AICPU_MSGQ_BASE + cpu_idx * AICPU_OFFSET;
+/*
+        for (size_t i = 0; i < EXE_TYPE_CNT; i++)
+        {
+            idx = 0;
+            for (size_t j = AIC_CNT_PER_THREAD * tid; j < AIC_CNT_PER_THREAD * (tid + 1); j++)
+            {
+                base = AICORE_SPR_BASE;
+                base += (i == 0 ? AICORE_CUBE_OFFSET : AICORE_VECTOR_OFFSET);
+                if (j >= AIC_CNT_PER_DIE) {
+                    base += AICORE_DIE_OFFSET + AICORE_OFFSET * (j - AIC_CNT_PER_DIE);
+                } else {
+                    base += AICORE_OFFSET * j;
+                }
+
+                g_ctrl_t[tid].aicore_spr_1[i][idx] = (uint64_t*)base;
+                g_ctrl_t[tid].aicore_spr_2[i][idx] = (uint64_t*)(base + AICORE_SPR_OFFSET); 
+                idx++;
+            }
+        }
+
+        for (size_t i = 0; i < EXE_TYPE_CNT; i++)
+        {        
+            hand_shake(tid, g_ctrl_t[tid].aicore_spr_1[i], i);
+            hand_shake((tid + 1), g_ctrl_t[tid].aicore_spr_2[i], i);
+        }
+*/
+
+
+static void hand_shake(int cpu_idx, uint64_t* aicore_spr[], int type, int ostd2_offset) {
+    uint64_t base = AICPU_MSGQ_BASE + cpu_idx * AICPU_OFFSET + ostd2_offset * AICPU_MSGQ_OFFSET;
     uint64_t msgq_addr = 0;
 
     for (size_t i = 0; i < AIC_CNT_PER_THREAD; i++)
     {
-        uint64_t offset = type == 0 ? 0 : 64;
+        uint64_t offset = type == 0 ? 0 : 128;
         msgq_addr = base + (i + offset)  * AICPU_MSGQ_OFFSET;
         // *aicore_spr[i] = HAND_SHAKE_VAL | (msgq_addr & LOAW_ADDR_MASK);
         WORKER_LOGF("cpu_idx,%d, index,%d, aicore_spr,%lx, msgq_addr,%lx", cpu_idx, i, aicore_spr[i], msgq_addr);
     }
 }
 
-static inline void get_free_exe(int tid)
+static inline void read_msgq(int tid)
 {
+    uint64_t msgq_value[4];
+
+    READ_REG(g_ctrl_t[tid].msg_bitmap[0][0], MSGQ_VLD0);
+    WRITE_REG(MSGQ_VLD0, g_ctrl_t[tid].msg_bitmap[0][0]);
+
+    READ_REG(g_ctrl_t[tid].msg_bitmap[0][1], MSGQ_VLD1);
+    WRITE_REG(MSGQ_VLD1, g_ctrl_t[tid].msg_bitmap[0][1]);
+
+    READ_REG(g_ctrl_t[tid].msg_bitmap[1][0], MSGQ_VLD2);
+    WRITE_REG(MSGQ_VLD2, g_ctrl_t[tid].msg_bitmap[1][0]);
+
+    READ_REG(g_ctrl_t[tid].msg_bitmap[1][1], MSGQ_VLD3);
+    WRITE_REG(MSGQ_VLD3, g_ctrl_t[tid].msg_bitmap[1][1]);
+
     for (int i = 0; i < EXE_TYPE_CNT; i++) {
         for (int j = 0; j < AIC_OSTD; j++) {
             g_ctrl_t[tid].free_bitmap[i][j] |= g_ctrl_t[tid].msg_bitmap[i][j];
@@ -164,15 +210,24 @@ static inline int send_task(ctrl_t *ctrl, int type)
         
         if (slot == 1) {
             ctrl->task_id_map2[type][idx] = task_id;
+            #ifdef REAL_CHIP
+            *ctrl->aicore_spr_2[idx] = task_id;
+            #endif
         } else {
+            
             ctrl->task_id_map1[type][idx] = task_id;
+            #ifdef REAL_CHIP
+            *ctrl->aicore_spr_1[idx] = task_id;
+            #endif
         }
         
         // Clear the free bit for this core/slot combination (mark as busy)
         ctrl->free_bitmap[type][slot] &= ~mask;
 
-        // Fake Return
+        #ifndef REAL_CHIP
         ctrl->msg_bitmap[type][slot] |= mask;
+        #endif
+        
         WORKER_LOGF("send,task_id,%u,core,%d,slot,%d,type,%d", task_id, core, slot, type);
         sent++;
         free_bitmap &= ~mask;
@@ -183,7 +238,7 @@ static inline int send_task(ctrl_t *ctrl, int type)
 int dispatch(int tid)
 {
     int total_sent = 0;
-    get_free_exe(tid);
+    read_msgq(tid);
     push_2_completed_queue(tid);
     total_sent += send_task(&g_ctrl_t[tid], TASK_TYPE_MIX);
     total_sent += send_task(&g_ctrl_t[tid], TASK_TYPE_VECTOR);
@@ -199,14 +254,15 @@ void *dispatch_worker(void *arg)
     int tid = (int)(intptr_t)arg;
     int total_sent = 0;
     WORKER_LOGF("dispatch,%d,start", tid);
+
     for (size_t i = 0; i < EXE_TYPE_CNT; i++)
     {        
-        hand_shake(tid, g_ctrl_t[tid].aicore_spr_1[i], i);
-        hand_shake((tid + 1), g_ctrl_t[tid].aicore_spr_2[i], i);
+        hand_shake(tid, g_ctrl_t[tid].aicore_spr_1[i], i, 0);
+        hand_shake(tid, g_ctrl_t[tid].aicore_spr_2[i], i, 64);
     }
 
-    atomic_store_explicit(&g_is_done, true, memory_order_release);
-    return NULL;
+    // atomic_store_explicit(&g_is_done, true, memory_order_release);
+    // return NULL;
 
     bool is_done = false;
     while (!is_done) {
